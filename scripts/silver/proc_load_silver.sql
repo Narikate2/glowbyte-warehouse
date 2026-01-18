c606980d058ef03e0bb4f940ad4fcade7f705035
Evgeny <rudy_excelent@mail.ru>
2026-01-18 21:08:42 +0300

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -48,20 +48,20 @@ begin
 			prd_end_dt
 		)
 		select 
-  		prd_id,
-  		replace(SUBSTRING(prd_key,1,5), '-', '_') category_id,
-  		SUBSTRING(prd_key, 7, len(prd_key)) prd_key,
-  		prd_nm,
-  		ISNULL(prd_cost, 0) prd_cost,
-  		case upper(trim(prd_line))
-  			when 'M' then 'Mountain'
-  			when 'R' then 'Road'
-  			when 'S' then 'Other Sales'
-  			when 'T' then 'Touring'
-  			else 'n/a'
-  		end as prd_line,
-  		prd_start_dt,
-  		dateadd(day, -1, lead(prd_start_dt) over(partition by prd_key order by prd_start_dt)) as prd_end_dt
+	  		prd_id,
+	  		replace(SUBSTRING(prd_key,1,5), '-', '_') category_id,
+	  		SUBSTRING(prd_key, 7, len(prd_key)) prd_key,
+	  		prd_nm,
+	  		ISNULL(prd_cost, 0) prd_cost,
+	  		case upper(trim(prd_line))
+	  			when 'M' then 'Mountain'
+	  			when 'R' then 'Road'
+	  			when 'S' then 'Other Sales'
+	  			when 'T' then 'Touring'
+	  			else 'n/a'
+	  		end as prd_line,
+	  		prd_start_dt,
+	  		dateadd(day, -1, lead(prd_start_dt) over(partition by prd_key order by prd_start_dt)) as prd_end_dt
 		from bronze.crm_prd_info;
 		set @end_time = getdate()
 
@@ -101,8 +101,8 @@ begin
 		from
 		(
 		select
-  		*,
-  		ROW_NUMBER() over(partition by cst_id order by cst_create_date desc) as flag_last
+	  		*,
+	  		ROW_NUMBER() over(partition by cst_id order by cst_create_date desc) as flag_last
 		from bronze.crm_cust_info
 		where cst_id is not null
 		) t where flag_last = 1;
@@ -130,27 +130,27 @@ begin
 		);
 
 		select 
-  		sls_ord_num,
-  		sls_prd_key,
-  		sls_cust_id,
-  		case when sls_order_dt = 0 or len(sls_order_dt) != 8 then null 
-  			else cast(cast(sls_order_dt as varchar) as date)
-  		end sls_order_dt,
-  			case when sls_ship_dt = 0 or len(sls_ship_dt) != 8 then null 
-  		else cast(cast(sls_ship_dt as varchar) as date)
-  		end sls_ship_dt,
-  		case when sls_due_dt = 0 or len(sls_due_dt) != 8 then null 
-  			else cast(cast(sls_due_dt as varchar) as date)
-  		end sls_due_dt,
-  		case when sls_sales is null or sls_sales <= 0 or sls_sales != sls_quantity * abs(sls_price)
-  			then sls_quantity * abs(sls_price)
-  			else sls_sales
-  		end as sls_sales,
-  		sls_quantity,
-  		case when sls_price is null or sls_price <= 0
-  			then sls_sales / nullif(sls_quantity, 0)
-  			else sls_price
-  		end as sls_price
+	  		sls_ord_num,
+	  		sls_prd_key,
+	  		sls_cust_id,
+	  		case when sls_order_dt = 0 or len(sls_order_dt) != 8 then null 
+	  			else cast(cast(sls_order_dt as varchar) as date)
+	  		end sls_order_dt,
+	  			case when sls_ship_dt = 0 or len(sls_ship_dt) != 8 then null 
+	  		else cast(cast(sls_ship_dt as varchar) as date)
+	  		end sls_ship_dt,
+	  		case when sls_due_dt = 0 or len(sls_due_dt) != 8 then null 
+	  			else cast(cast(sls_due_dt as varchar) as date)
+	  		end sls_due_dt,
+	  		case when sls_sales is null or sls_sales <= 0 or sls_sales != sls_quantity * abs(sls_price)
+	  			then sls_quantity * abs(sls_price)
+	  			else sls_sales
+	  		end as sls_sales,
+	  		sls_quantity,
+	  		case when sls_price is null or sls_price <= 0
+	  			then sls_sales / nullif(sls_quantity, 0)
+	  			else sls_price
+	  		end as sls_price
 		from bronze.crm_sales_details;
 
 		set @end_time = getdate()
@@ -175,17 +175,17 @@ begin
 		)
 
 		select
-  		case when upper(cid) like 'NAS%' then substring(cid, 4, len(cid))
-  			else upper(cid)
-  		end as cid,
-  		case when bdate > getdate() then null
-  			else bdate
-  		end as bdate,
-  		case 
-  			when upper(trim(gen)) in ('F', 'FEMALE') then 'Female'
-  			when upper(trim(gen)) in ('M', 'Male') then 'Male'
-  			else 'n/a'
-  		end as gen
+	  		case when upper(cid) like 'NAS%' then substring(cid, 4, len(cid))
+	  			else upper(cid)
+	  		end as cid,
+	  		case when bdate > getdate() then null
+	  			else bdate
+	  		end as bdate,
+	  		case 
+	  			when upper(trim(gen)) in ('F', 'FEMALE') then 'Female'
+	  			when upper(trim(gen)) in ('M', 'Male') then 'Male'
+	  			else 'n/a'
+	  		end as gen
 		from bronze.erp_cust_az12;
 
 		set @end_time = getdate()
@@ -203,12 +203,12 @@ begin
 			(cid, cntry)
 
 		select 
-  		replace(cid, '-', '') cid,
-  		case when trim(cntry) = 'DE' then 'Germany'
-  			 when trim(cntry) IN ('US', 'USA') then 'United States'
-  			 when trim(cntry) = '' or cntry is null then 'n/a'
-  			 else trim(cntry)
-  		end as cntry
+	  		replace(cid, '-', '') cid,
+	  		case when trim(cntry) = 'DE' then 'Germany'
+	  			 when trim(cntry) IN ('US', 'USA') then 'United States'
+	  			 when trim(cntry) = '' or cntry is null then 'n/a'
+	  			 else trim(cntry)
+	  		end as cntry
 		from bronze.erp_loc_a101;
 		set @end_time = getdate()
 		print '>> load duration: ' + cast(Datediff(second, @start_time, @end_time) as nvarchar) + ' seconds'
